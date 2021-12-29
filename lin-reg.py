a4094dfe83983c9b2c557784a3957c464dc9852a
ImGilbes <gianlucasax@hotmail.it>
2021-12-29 20:44:13 +0000

    nicely done linear regression

---
+++ lin-reg.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# create random dataset
+xnp, ynp = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=1)
+x = torch.from_numpy(xnp.astype(np.float32))
+y = torch.from_numpy(ynp.astype(np.float32))
+
+#now x and y contain only one row, whereas they should be a column vector
+y = y.view(y.shape[0], 1) #now y is a column vector [[][][]]
+
+samples, features = x.shape
+
+insize = features
+outsize = 1
+model = nn.Linear(insize, outsize)
+
+learning_rate = 0.1
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+nepochs = 100
+for epoch in range(nepochs):
+
+    prediction = model(x)
+    error = loss(prediction, y)
+
+    #backprop
+    error.backward()
+
+    #update
+    optimizer.step()
+
+    optimizer.zero_grad()
+
+    if epoch %(nepochs/10) == 0:
+        print(f'epoch: {epoch}, loss= {error.item():.8f}')
+
+
+pred = model(x).detach().numpy() #generates a new tensor without grandient required thing
+plt.plot(xnp, ynp, 'ro')
+plt.plot(xnp, pred, 'b')
+plt.show()
+
+
+
+
+
--- simple-sgd.py
+++ simple-sgd.py
@@ -1,11 +1,31 @@
 import torch
 import torch.nn as nn
 
-x = torch.tensor([1,2,3,4], dtype=torch.float32)
-# y = torch.tensor([1,2,3,4], dtype=torch.float32) #expected w = 1
-y = torch.tensor([2,4,6,8], dtype=torch.float32) #expected w = 2
-# y = torch.tensor([23, 11 , 20, 4], dtype=torch.float32) #mess with the expected w
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True) #requires gradient if you use only torch
+#now tensors have to be 2d
+x = torch.tensor([[1],[2],[3],[4]], dtype=torch.float32)
+y = torch.tensor([[1],[2],[3],[4]], dtype=torch.float32) #expected w = 2
+
+#the input to a model always has to be a tensor
+x_test = torch.tensor([5], dtype=torch.float32)
+
+n_samples, n_features = x.shape
+print(n_samples,n_features)
+
+#THE FORWARD FUNCTION NOW BECOMES A MODEL!
+input_size = n_features
+output_size = n_features
+# model = nn.Linear(input_size, output_size)
+
+#create my custom model class by inheriting another model 
+class LinearRegression(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegression, self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
+
+model = LinearRegression(input_size ,output_size)
 
 learning_rate = 0.01
 iter = 100
@@ -13,17 +33,16 @@ iter = 100
 loss = nn.MSELoss() #the loss is not defined manually (Mean Squared Error)
 
 #optimizer = torch.optim.SGD([w], lr=learning_rate, momentum=0.9)
-optimizer = torch.optim.SGD([w], lr=learning_rate)
-
-def forward(x):
-    return w * x
+#the parameters that sgd optimizes are passed as a list!
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 
-print(f'Only Pytorch: prediction before training: f(5)={forward(5)}')
+
+print(f'Only Pytorch: prediction before training: f(5)={model(x_test)}')
 
 for epoch in range(iter):
 
-    y_pred = forward(x)
+    y_pred = model(x)
     
     tmp_loss = loss(y, y_pred)
     
@@ -36,6 +55,7 @@ for epoch in range(iter):
     optimizer.zero_grad()
 
     if epoch % (iter/10) == 0:
-        print(f'{epoch}: w ={w}, loss ={tmp_loss:.8f}')
+        [w, b] = model.parameters()
+        print(f'{epoch}: w ={w[0][0].item()}, loss ={tmp_loss:.8f}')
         
-print(f'Only Pytorch: prediction after training: f(5)={forward(5)}')
+print(f'Only Pytorch: prediction after training: f(5)={model(x_test)}')
